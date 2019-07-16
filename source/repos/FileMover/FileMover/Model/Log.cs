ac3317905afa7f2abf72eee41b381d00c40fac29
Patrick Ciocson <princepatrickz@gmail.com>
2019-07-16 11:21:15 +0800

    new

---
+++ source/repos/FileMover/FileMover/Model/Log.cs
@@ -0,0 +1,14 @@
+﻿using FileMover.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMover.Model
+{
+    public class Log : ViewModelBase
+    {
+        public string Logs { get; set; }
+    }
+}
---
+++ source/repos/FileMover/FileMover/Utility/SystemControls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMover.Utility
+{
+    public class SystemControls
+    {
+        [DllImport("user32.dll")]
+        internal static extern int GetWindowLong(IntPtr hWnd, int index);
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowLong(IntPtr hWnd, int index, int newLong);
+    }
+}
